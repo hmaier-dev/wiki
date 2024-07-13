c9dc81eec79d800f4f08c7caa189926d73b5a7f6
Hendrik Maier <hmaier@ipb-halle.de>
2024-07-13 18:17:25 +0200

    feat: censoring articles to build public index.wiki

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,9 @@
-import sys 
+import sys
+import os
+import re
 
+
+# 1st/2nd empty line are used as delimiters
 def extract_table_of_contents(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -14,15 +18,35 @@ def extract_table_of_contents(file_path):
                 second_empty_line = i
                 break
 
-    # Extract the code block between the first and second empty lines
     if first_empty_line is not None and second_empty_line is not None:
         code_block = lines[first_empty_line + 1: second_empty_line]
-        return ''.join(code_block)
+        return code_block
     else:
         return None
 
-public = sys.argv[1]
-private = sys.argv[2]
 
-private = extract_table_of_contents(public)
-print(private)
+# Just use the present articles names as links
+def censor_table_of_contents(private_toc):
+    censored = []
+    public = ""
+    f = os.listdir(".")
+    wiki_files = [file for file in f if file.endswith('.wiki')]
+    pattern = "\*\s\[\[(.*)\]\]|\*\s(.*)"
+    for line in private_toc:
+        matches = re.findall(pattern, line)
+        match = results = [match[0] if match[0] else match[1] for match in matches]
+        if f"{match[0]}.wiki" in wiki_files:
+            public = public + line
+        else:
+            censored.append(line)
+    return public, censored
+
+
+private = sys.argv[1]
+public_name = sys.argv[2]
+
+toc = extract_table_of_contents(private)
+public, censored = censor_table_of_contents(toc)
+
+print(censored)
+print(public)
